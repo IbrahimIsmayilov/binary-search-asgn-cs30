42ca0bd530cb198eca52a208d7bba89523406024
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2023-02-28 07:50:21 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+nums = [10, 30, 40, 45, 70, 80, 85, 90, 100]
+words = ["at", "ball", "cat", "dog", "eye", "fish", "good"]
+unsorted = [30, 20, 70, 40, 50, 100, 90]
+
+import math
+
+def binarySearch(anArray, item):
+    li = 0
+    ui = len(anArray) - 1
+    leftValues = True
+
+    while leftValues:
+        mi = math.floor((li + ui) / 2)
+        if anArray[mi] == item:
+            leftValues = False
+            return mi
+        elif item < mi:
+            ui = mi - 1
+        else:
+            li = mi + 1
+
+    return -1
+        
+
+
+print(binarySearch(nums, 100))
+print(binarySearch(nums, 75))
+# print(binarySearch(words, "fish"))
+# print(binarySearch(words, "at"))
+# print(binarySearch(unsorted, 70))
